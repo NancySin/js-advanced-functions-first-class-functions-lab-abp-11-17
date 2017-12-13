699b83a34fe61b4449fab2dc9155324168e38a11
nancy sin <nsin@sbcglobal.net>
2017-12-13 21:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function(drivers){ return drivers.slice(-2)}
 
 const selectingDrivers=
 [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (integer) {
+  return function (fare) {
+     return fare * integer
+   }}
