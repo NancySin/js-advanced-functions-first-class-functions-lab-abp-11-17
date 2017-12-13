7e916fa8857b9e2a02955298820a44b5fadc1e9e
nancy sin <nsin@sbcglobal.net>
2017-12-13 21:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ const createFareMultiplier = function (integer) {
   return function (fare) {
      return fare * integer
    }}
+const fareDoubler() = createFareMultiplier(2)
+
+const  fareTripler() = createFareMultiplier(3)
