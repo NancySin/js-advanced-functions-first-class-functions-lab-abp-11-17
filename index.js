f19654f0952a3390b6aa1a0c5498c5f92baf685e
nancy sin <nsin@sbcglobal.net>
2017-12-13 20:39:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const returnFirstTwoDrivers=
 function (drivers){ return drivers.slice(0,2)}
 
 const returnLastTwoDrivers=
-function(drivers){ return drivers.slice(drivers.length-2, driver.length)}
+function(drivers){ return drivers.slice(-2)}
